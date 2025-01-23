fafbe08ab39d7ebb47bacc988424fad03b72cdb5
Nitesh <nitesh.nky25@gmail.com>
2025-01-23 00:38:26 -0500

    essage

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,9 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // Import Register component
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers'; // Import Dealers component
+import Dealer from "./components/Dealers/Dealer"; // Import Dealer component
+import PostReview from "./components/Dealers/PostReview"; // Import PostReview component
 
 function App() {
   return (
@@ -10,6 +13,15 @@ function App() {
 
       {/* Route for Register page */}
       <Route path="/register" element={<Register />} />
+
+      {/* Route for Dealers page */}
+      <Route path="/dealers" element={<Dealers />} />
+
+      {/* Route for Dealer-specific page with reviews */}
+      <Route path="/dealer/:id" element={<Dealer />} />
+
+      {/* Route for posting a review for a dealer */}
+      <Route path="/postreview/:id" element={<PostReview />} />
     </Routes>
   );
 }
