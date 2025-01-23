59561d309efde80bb5ae1e9cc2e19d94d7440539
Nitesh <nitesh.nky25@gmail.com>
2025-01-22 19:42:17 -0500

    message

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Import Register component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
+      {/* Route for Login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Route for Register page */}
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
